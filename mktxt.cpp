a517344c089a99f5b26d180c2fbc4fedf396e09e
pooriayousefi <pooriayousefi@aol.com>
2025-04-10 12:24:21 +0330

    Add project files.

---
+++ mktxt.cpp
@@ -0,0 +1,18 @@
+﻿#include "mktxt.h"
+int main()
+{
+	std::string characters{ "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz" };
+	std::random_device rd{};
+	std::mt19937_64 rng{ rd() };
+	std::uniform_int_distribution<int> name_length_dist{ 8, 16 };
+	std::uniform_int_distribution<int> name_dist{ 0, static_cast<int>(characters.size()) - 1 };
+	std::string text_file_name(name_length_dist(rng), '?');
+	for (char& c : text_file_name)
+	{
+		c = characters[name_dist(rng)];
+	}
+	std::ofstream text_file{ text_file_name + ".txt" };
+	if (text_file.is_open())
+		text_file.close();
+	return 0;
+}
